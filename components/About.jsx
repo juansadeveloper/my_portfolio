68855eb25fa7c9a04d28c6d4a6a34caf8919e23b
juansadeveloper <juansapereira@hotmail.com>
2023-05-18 09:01:39 +0100

    More changes
    I changed the way I import the profile.pic.jpg image and also moved it to public directory.

--- components/About.jsx
+++ components/About.jsx
@@ -10,16 +10,15 @@ function About({ changeAbout}) {
         <h2>About me</h2>
         <p>I have 2 years of experience in web development.</p>   
         <p>I´m a creative person who wants to add value to the world.</p>
-        {/* <motion.div
+        <motion.div
 
           initial={{ opacity: 0}}
           whileInView={{ opacity: 1}}
           viewport={{ once: true }}
           transition={{ duration: 5}}        >
-          <Image src={profileimg} alt="profile-pic" className='profile-img' onClick={changeAbout}></Image>
-        </motion.div> */}
-          {/* <Image src={profileimg} alt="profile-pic" className='profile-img' onClick={changeAbout}
-          ></Image> */}
+          <Image src="/profile-pic.jpg" alt="profile-pic" priority={false} width={100} height={100} className='profile-img' onClick={changeAbout}></Image>
+        </motion.div>
+          {/* n */}
         
         <p >I am always excited to work on new projects, message me and let's talk.</p>
         <Link  to="contact" smooth={true} offset={0} duration={700}><button className='normal-button'>Contact Me</button></Link>
