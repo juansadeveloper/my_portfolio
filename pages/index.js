9491240a4693c44c5292f8b7c3367794847c999d
juansadeveloper <juansapereira@hotmail.com>
2023-05-22 07:54:57 +0100

    Update index.js
    I changed the title of the website

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Santiago</title>
+        <title>Santiago Pereira</title>
         <meta name="description" content="Portfolio Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
