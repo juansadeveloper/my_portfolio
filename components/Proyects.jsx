93821974cccf5c6be41c128e914168f1e5abc0e1
juansadeveloper <juansapereira@hotmail.com>
2024-06-28 21:12:08 +0200

    updated

--- components/Proyects.jsx
+++ components/Proyects.jsx
@@ -104,7 +104,7 @@ function Proyects() {
           </div>
           <div className='proyects-contact'>
             <p style={{fontWeight: "600"}}>Want to make a project together?</p>
-            <Link  to="contact" smooth={true} offset={0} duration={700}><button>Let me know</button></Link>
+            <Link  to="contact" smooth={true} offset={0} duration={700}><button className='text'>Let me know</button></Link>
           </div>
         </div>
     </div>
